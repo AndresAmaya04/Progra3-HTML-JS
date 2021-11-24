be04f8ca9d7bbc27abe1f1a40ef723880f85c4bf
Andres Amaya <leon.amaya270@gmail.com>
2021-11-24 17:05:12 -0500

    Agrego los archivos js y html

---
+++ scriptMoment.js
@@ -0,0 +1,19 @@
+console.log("Secrip create")
+
+var form1 = document.getElementById("form1");
+
+form1.addEventListener("submit", function(event){
+	event.preventDefault();
+	console.log(event);
+	obtainData();
+});
+
+function obtainData(){
+	var data = Object.fromEntries(new FormData(event.target).entries());
+
+	var output = document.getElementById("jsonHtml");
+
+	output.innerHTML = '<h3 style="color: rgb(69, 182, 224)" >' + data.i_name + " " + data.i_lastname + " " + data.I_date + "</h3>";
+}
+
+
