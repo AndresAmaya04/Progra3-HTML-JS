b1718cee8006c7fe5ef7f9e948ce375c77f3602d
Andres Amaya <leon.amaya270@gmail.com>
2021-11-30 17:37:44 -0500

    Cambios a HTML y a CSS

--- scriptMoment.js
+++ scriptMoment.js
@@ -2,11 +2,18 @@ console.log("Secrip create")
 
 var form1 = document.getElementById("form1");
 
+
 form1.addEventListener("submit", function(event){
 	event.preventDefault();
 	console.log(event);
 	obtainData();
 });
+
+function change() {
+    var item = document.getElementById("item_change");
+
+    item.style.backgroundColor = "red";
+}
 
 function obtainData(){
 	var data = Object.fromEntries(new FormData(event.target).entries());
@@ -15,5 +22,6 @@ function obtainData(){
 
 	output.innerHTML = '<h3 style="color: rgb(69, 182, 224)" >' + data.i_name + " " + data.i_lastname + " " + data.I_date + "</h3>";
 }
+
 
 
